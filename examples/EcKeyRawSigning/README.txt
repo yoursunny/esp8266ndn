046a1564d1c0f124dea0ed8166c981c9c2b11b60
Junxiao Shi <git@mail1.yoursunny.com>
2017-12-21 19:01:25 -0700

    examples: EcKeyRawSigning

---
+++ examples/EcKeyRawSigning/README.txt
@@ -0,0 +1,5 @@
+This example demonstrates how to use EcKey class to sign/verify messages directly.
+The signing parameters are compatible with ndn-cxx:commit:7f01247b5bc75d9416e22828b72976abd57f16a9.
+ndn-cxx/ec-demo.cpp has an ndn-cxx program that generates an EC key and then sign/verify messages.
+The sketch and the ndn-cxx program can be used together to check compatibility.
+See https://youtu.be/Ko0J5kch5W8 for a video demo.
---
+++ examples/EcKeyRawSigning/ndn-cxx/ec-demo.cpp
@@ -0,0 +1,115 @@
+// g++ -std=c++11 -o ec-demo ec-demo.cpp $(pkg-config --cflags --libs libndn-cxx)
+
+#include <cinttypes>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+#include <ndn-cxx/security/transform.hpp>
+#include <ndn-cxx/security/verification-helpers.hpp>
+#include <ndn-cxx/security/v2/key-chain.hpp>
+#include <ndn-cxx/util/indented-stream.hpp>
+
+bool readKeyBits(FILE* input, uint8_t* output, int len) {
+  for (int i = 0; i < len; ++i) {
+    if (fscanf(input, " %2" SCNx8 "%*[:]", output + i) != 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void printHexArray(std::ostream& os, const std::string& var, const uint8_t* buf, int len) {
+  os << "const uint8_t " << var << "[] {\n";
+
+  ndn::util::IndentedStream osi(os, "  ");
+  osi << std::hex << std::uppercase << std::setfill('0');
+  for (int i = 0; i < len; ++i) {
+    osi << "0x" << std::setw(2) << static_cast<int>(buf[i]) << ',' << (i % 16 == 15 ? '\n' : ' ');
+  }
+  osi.flush();
+
+  os << "};\n";
+}
+
+void printHexLine(std::ostream& os, const std::string& var, const uint8_t* buf, int len) {
+  os << var << "= ";
+
+  using namespace ndn::security::transform;
+  StepSource ss;
+  ss >> hexEncode(true) >> streamSink(os);
+  ss.write(buf, len);
+  ss.end();
+
+  os << '\n';
+}
+
+int main() {
+  // generate key
+  ndn::KeyChain keyChain("pib-memory:", "tpm-memory:");
+  auto identity = keyChain.createIdentity("/ec-demo", ndn::EcKeyParams(256));
+
+  // export key
+  const std::string password = "0000";
+  auto safeBag = keyChain.exportSafeBag(identity.getDefaultKey().getDefaultCertificate(), password.data(), password.size());
+  auto keyBag = safeBag->getEncryptedKeyBag();
+  std::string pkcs8File = std::tmpnam(nullptr);
+  std::ofstream pkcs8Stream(pkcs8File, std::ios_base::binary);
+  pkcs8Stream.write(reinterpret_cast<const char*>(keyBag.data()), keyBag.size());
+  pkcs8Stream.close();
+
+  // extract key bits with OpenSSL
+  FILE* openssl = popen(("openssl pkcs8 -inform DER -in " + pkcs8File + " -passin pass:" + password + " | openssl ec -noout -text 2>/dev/null; rm -f " + pkcs8File).data(), "r");
+  uint8_t pvtkeyBits[32], pubkeyBits[64];
+  char posTest[2];
+  if (!(fscanf(openssl, "Private-Key: (256 bit) priv: ") == 0 &&
+        readKeyBits(openssl, pvtkeyBits, sizeof(pvtkeyBits)) &&
+        fscanf(openssl, " pu%1[b]: 04:", posTest) == 1 &&
+        readKeyBits(openssl, pubkeyBits, sizeof(pubkeyBits)) &&
+        fscanf(openssl, " ASN1 OI%1[D]:", posTest) == 1)) {
+    return 3;
+  }
+
+  // print key as array for use in code
+  printHexArray(std::cout, "PVTKEY", pvtkeyBits, sizeof(pvtkeyBits));
+  printHexArray(std::cout, "PUBKEY", pubkeyBits, sizeof(pubkeyBits));
+
+  // print key as line for use in demo program
+  printHexLine(std::cout, "pvtkey", pvtkeyBits, sizeof(pvtkeyBits));
+  printHexLine(std::cout, "pubkey", pubkeyBits, sizeof(pubkeyBits));
+
+  // print input
+  std::string input = "yoursunny.com esp8266ndn ECDSA signing demo";
+  printHexLine(std::cout, "input", reinterpret_cast<const uint8_t*>(input.data()), input.size());
+
+  auto pubkey = identity.getDefaultKey().getPublicKey();
+  while (true) {
+    // print ndn-cxx signature
+    auto sig = keyChain.sign(reinterpret_cast<const uint8_t*>(input.data()), input.size());
+    sig.encode();
+    printHexLine(std::cout, "pc-sig", sig.value(), sig.value_size());
+
+    // input and verify esp8266ndn signature
+    std::cout << "esp-sig= ";
+    char espSigLine[150];
+    std::cin.getline(espSigLine, sizeof(espSigLine));
+    std::string espSig;
+    {
+      std::istringstream is(espSigLine);
+      std::ostringstream os;
+      namespace t = ndn::security::transform;
+      try {
+        t::streamSource(is) >> t::hexDecode() >> t::streamSink(os);
+      } catch (const t::Error&) {}
+      espSig = os.str();
+    }
+
+    bool res = ndn::security::verifySignature(reinterpret_cast<const uint8_t*>(input.data()), input.size(), reinterpret_cast<const uint8_t*>(espSig.data()), espSig.size(), pubkey.data(), pubkey.size());
+    std::cout << (res ? "res=OK\n" : "res=BAD\n");
+  }
+}
