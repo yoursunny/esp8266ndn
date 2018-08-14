66b2adf936647ffbb2c5d846db04cd25b93e94a7
Junxiao Shi <git@mail1.yoursunny.com>
2018-08-14 14:20:08 -0400

    app: don't crash PingServer when probeCb is unset

--- src/app/ping-server.cpp
+++ src/app/ping-server.cpp
@@ -9,6 +9,7 @@ namespace ndn {
 PingServer::PingServer(Face& face, const NameLite& prefix)
   : m_face(face)
   , m_prefix(prefix)
+  , m_probeCb(nullptr)
 {
 }
 
@@ -27,7 +28,9 @@ PingServer::processInterest(const InterestLite& interest, uint64_t endpointId)
 
   uint8_t payload[NDNPINGSERVER_PAYLOAD_MAX];
   size_t payloadSize = 0;
-  m_probeCb(m_probeCbArg, interest, payload, &payloadSize);
+  if (m_probeCb != nullptr) {
+    m_probeCb(m_probeCbArg, interest, payload, &payloadSize);
+  }
   if (payloadSize > NDNPINGSERVER_PAYLOAD_MAX) {
     PINGSERVER_DBG(F("payload too large ") << payloadSize);
     return false;
