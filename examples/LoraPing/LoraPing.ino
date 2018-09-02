d1e57ab50f045a22679531a1c27801f637d65507
Junxiao Shi <git@mail1.yoursunny.com>
2018-09-01 22:17:40 -0400

    examples: LoraPing allows disabling client

--- examples/LoraPing/LoraPing.ino
+++ examples/LoraPing/LoraPing.ino
@@ -1,15 +1,7 @@
-// LoRa frequency reference
-// https://www.thethingsnetwork.org/docs/lorawan/frequency-plans.html
+#include <esp8266ndn.h>
 
 // Heltec LoRa library
 // https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series/tree/master/esp32/libraries/LoRa
-#define LORA_BEGIN LoRa.begin(915E6, true)
-
-// Arduino LoRa library
-// https://github.com/sandeepmistry/arduino-LoRa/
-// #define LORA_BEGIN LoRa.begin(915E6)
-
-#include <esp8266ndn.h>
 #include <LoRa.h>
 
 ndn::DigestKey g_key;
@@ -22,17 +14,22 @@ ndn::PingServer g_server(g_face, g_sPrefix);
 char CPREFIX[] = "/example/esp32/lora/ping";
 ndn_NameComponent g_cComps[5];
 ndn::InterestLite g_cInterest(g_cComps, 5, nullptr, 0, nullptr, 0);
-ndn::PingClient g_client(g_face, g_cInterest, {2500, 500});
+ndn::PingClient g_client(g_face, g_cInterest, {3000, 500});
+bool g_enableClient = true;
 
 void
 serverProbe(void* arg, const ndn::InterestLite& interest, uint8_t* payloadBuf, size_t* payloadSize)
 {
+  Serial.printf("S RSSI=%d SNR=%f\n", LoRa.packetRssi(), LoRa.packetSnr());
 }
 
 void
 clientEvent(void* arg, ndn::PingClient::Event evt, uint64_t seq)
 {
   digitalWrite(LED_BUILTIN, evt == ndn::PingClient::Event::PROBE ? HIGH : LOW);
+  if (evt == ndn::PingClient::Event::RESPONSE) {
+    Serial.printf("C RSSI=%d SNR=%f\n", LoRa.packetRssi(), LoRa.packetSnr());
+  }
 }
 
 void
@@ -41,9 +38,18 @@ setup()
   Serial.begin(115200);
   Serial.println();
   ndn::setLogOutput(Serial);
-
+  pinMode(KEY_BUILTIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  LORA_BEGIN;
+
+  // initialize LoRa
+  LoRa.setTxPower(12);
+  LoRa.setSpreadingFactor(10);
+  LoRa.setSignalBandwidth(250E3);
+  LoRa.setCodingRate4(8);
+  LoRa.setPreambleLength(16);
+  LoRa.setSyncWord(0x12);
+  LoRa.crc();
+  LoRa.begin(915E6, true);
 
   // LoRa MTU is 255, no need for the default 1500-octet buffer
   {
@@ -69,6 +75,18 @@ void
 loop()
 {
   g_face.loop();
-  g_client.loop();
-  delay(10);
+
+  if (digitalRead(KEY_BUILTIN) == LOW) {
+    g_enableClient = !g_enableClient;
+    Serial.println(g_enableClient ? "Enabling client" : "Disabling client");
+    while (digitalRead(KEY_BUILTIN) == LOW) // wait for key release
+      ;
+  }
+
+  if (g_enableClient) {
+    g_client.loop();
+  }
+  else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
