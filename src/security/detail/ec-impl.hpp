78ed72f2d1f9408110809414c771b24494bac16b
Junxiao Shi <git@mail1.yoursunny.com>
2018-09-23 22:36:15 -0400

    security: EC keygen from MbedTLS on ESP32

--- src/security/detail/ec-impl.hpp
+++ src/security/detail/ec-impl.hpp
@@ -7,6 +7,10 @@
 #include "ec-impl-microecc.hpp"
 #endif
 
+#if defined(ESP8266)
 #include "eckeygen-impl-microecc.hpp"
+#else
+#include "eckeygen-impl-mbedtls.hpp"
+#endif
 
 #endif // ESP8266NDN_EC_IMPL_HPP
