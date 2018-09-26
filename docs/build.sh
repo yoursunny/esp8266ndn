39559099a6f92ef41298e3d235298bc9613923e6
Junxiao Shi <git@mail1.yoursunny.com>
2018-09-25 23:16:41 -0400

    docs: Doxyfile

---
+++ docs/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if ! [[ -x ./doxygen.exe ]]; then
+  if [[ "$OS" == "Windows_NT" ]]; then
+    curl ftp://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.windows.x64.bin.zip > doxygen-windows.zip
+    unzip doxygen-windows.zip doxygen.exe libclang.dll
+    rm doxygen-windows.zip
+  elif which doxygen >/dev/null; then
+    ln -s $(which doxygen) ./doxygen.exe
+  else
+    echo "Doxygen is unavailable" >/dev/stderr
+    exit 1
+  fi
+fi
+./doxygen.exe
