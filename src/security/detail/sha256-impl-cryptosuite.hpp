38a4072b36ef52893ad24b08de662fbe26676262
Junxiao Shi <git@mail1.yoursunny.com>
2019-03-10 12:18:28 -0400

    security: sha256-impl-cryptosuite
    Cryptosuite downloaded from
    https://github.com/simonratner/Arduino-SHA-256
    commit b4b722e67de6c6f35ab358493e676971f96e32de
    
    Modifications:
    * change filename
    * delete initHmac_EEPROM function
    * fix pgmspace.h path for ESP8266

---
+++ src/security/detail/sha256-impl-cryptosuite.hpp
@@ -0,0 +1,41 @@
+#ifndef ESP8266NDN_SHA256_IMPL_CRYPTOSUITE_HPP
+#define ESP8266NDN_SHA256_IMPL_CRYPTOSUITE_HPP
+
+#include "cryptosuite-sha256.h"
+
+namespace ndn {
+namespace detail {
+
+static void
+sha256(const uint8_t* input, size_t len, uint8_t* result)
+{
+  Sha256 sha;
+  sha.init();
+  sha.write(input, len);
+  memcpy(result, sha.result(), HASH_LENGTH);
+}
+
+class HmacImpl
+{
+public:
+  HmacImpl(const uint8_t* secret, size_t secretLen)
+  {
+    m_sha.initHmac(secret, secretLen);
+  }
+
+  void
+  computeHmac(const uint8_t* input, size_t inputLen, uint8_t* sig) const
+  {
+    m_sha.reset();
+    m_sha.write(input, inputLen);
+    memcpy(sig, m_sha.resultHmac(), HASH_LENGTH);
+  }
+
+private:
+  mutable Sha256 m_sha;
+};
+
+} // namespace detail
+} // namespace ndn
+
+#endif // ESP8266NDN_SHA256_IMPL_CRYPTOSUITE_HPP
--- src/security/detail/sha256-impl.hpp
+++ src/security/detail/sha256-impl.hpp
@@ -1,12 +1,12 @@
-#ifndef ESP8266NDN_HMAC_IMPL_HPP
-#define ESP8266NDN_HMAC_IMPL_HPP
+#ifndef ESP8266NDN_SHA256_IMPL_HPP
+#define ESP8266NDN_SHA256_IMPL_HPP
 
 #if defined(ESP8266)
 #include "sha256-impl-bearssl.hpp"
 #elif defined(ESP32)
 #include "sha256-impl-mbedtls.hpp"
 #else
-#include "sha256-impl-none.hpp"
+#include "sha256-impl-cryptosuite.hpp"
 #endif
 
-#endif // ESP8266NDN_HMAC_IMPL_HPP
+#endif // ESP8266NDN_SHA256_IMPL_HPP
