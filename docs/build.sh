ee7652502bdbd44efb7b0ca21af9c4cbd8f61ede
Junxiao Shi <git@mail1.yoursunny.com>
2020-07-25 08:17:53 -0400

    mk: change Google Analytics code

--- docs/build.sh
+++ docs/build.sh
@@ -7,7 +7,7 @@ if ! [[ -x ./doxygen.exe ]]; then
   if [[ "$OS" == "Windows_NT" ]]; then
     curl -sL https://downloads.sourceforge.net/project/doxygen/rel-1.8.17/doxygen-1.8.17.windows.x64.bin.zip > doxygen-windows.zip
     unzip doxygen-windows.zip doxygen.exe libclang.dll
-    # rm doxygen-windows.zip
+    rm doxygen-windows.zip
   elif which doxygen >/dev/null; then
     ln -s $(which doxygen) ./doxygen.exe
   else
@@ -20,5 +20,5 @@ fi
 if [[ -n $GTAGID ]]; then
   GTAG='<script async src="https://www.googletagmanager.com/gtag/js?id='$GTAGID'"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag("js",new Date());gtag("config","'$GTAGID'");</script>'
   find html -name '*.html' | xargs sed -i "s#</head>#$GTAG</head>#"
-  cp _redirects html/
 fi
+cp _redirects html/
