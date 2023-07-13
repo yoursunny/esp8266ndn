9af878bfcffc5959ede3e02bb51e84c67c658410
Junxiao Shi <git@mail1.yoursunny.com>
2023-07-13 00:37:34 -0400

    docs: Google Analytics 4

--- docs/build.sh
+++ docs/build.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-set -eo pipefail
+set -euo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-if ! [[ -x ./doxygen.exe ]]; then
-  rm -f ./doxygen.exe
-  if [[ "$OS" == "Windows_NT" ]]; then
-    curl -sfL https://www.doxygen.nl/files/doxygen-1.9.2.windows.x64.bin.zip > doxygen-windows.zip
-    unzip -o doxygen-windows.zip doxygen.exe libclang.dll
-    rm doxygen-windows.zip
-  elif which doxygen >/dev/null; then
-    ln -s $(which doxygen) ./doxygen.exe
-  else
-    echo "Doxygen is unavailable" >/dev/stderr
-    exit 1
-  fi
-fi
-./doxygen.exe Doxyfile 2>&1 | ./filter-Doxygen-warning.awk 1>&2
+doxygen Doxyfile 2>&1 | ./filter-Doxygen-warning.awk 1>&2
 
-find html -name '*.html' | xargs sed -i '/<\/head>/ i\ <script>(function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)};a=t.createElement(i);c=t.getElementsByTagName(i)[0];a.async=true;a.src=s;c.parentNode.insertBefore(a,c)})(window,document,"galite","script","https://cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js"); if (location.hostname.endsWith(".ndn.today")) { galite("create", "UA-935676-11", "auto"); galite("send", "pageview"); }</script>'
+find html -name '*.html' | xargs sed -i '/<\/head>/ i\<script async src="https://www.googletagmanager.com/gtag/js?id=G-YSW3MP43Z4"></script><script>window.dataLayer=[];function gtag(){dataLayer.push(arguments);}if(location.hostname.endsWith(".ndn.today")){gtag("js",new Date());gtag("config","G-YSW3MP43Z4");}</script></script>'
 cp _redirects html/
